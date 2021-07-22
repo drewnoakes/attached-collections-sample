8644e5189a6bc53ac4b2d1d8e1f27e3f1a74f8c3
Drew Noakes <git@drewnoakes.com>
2021-07-22 11:15:51 +1000

    Use better state icons and document other image types

--- AttachedCollectionsSample/SampleAttachedItem.cs
+++ AttachedCollectionsSample/SampleAttachedItem.cs
@@ -49,10 +49,48 @@ namespace AttachedCollectionsSample
 
         public ImageMoniker StateIconMoniker => SelectRandom(s_states);
 
-        private static readonly ImageMoniker[] s_monikers = { KnownMonikers.AboutBox, KnownMonikers.PageLayout, KnownMonikers.ShowStartWindow, KnownMonikers.TableOK };
-        private static readonly ImageMoniker[] s_overlays = { KnownMonikers.OverlayAlert, KnownMonikers.OverlayError, KnownMonikers.OverlayExcluded, KnownMonikers.OverlayFriend, KnownMonikers.OverlayLock, KnownMonikers.OverlayLoginDisabled, KnownMonikers.OverlayWarning };
-        private static readonly ImageMoniker[] s_states = { KnownMonikers.CheckedIn, KnownMonikers.CheckedOutForEdit, KnownMonikers.CheckedOutByOtherUser };
+        // A subset of known monikers, for demo purposes
+        private static readonly ImageMoniker[] s_monikers =
+        {
+            KnownMonikers.AboutBox,
+            KnownMonikers.PageLayout,
+            KnownMonikers.ShowStartWindow,
+            KnownMonikers.TableOK
+        };
+
+        // For the overlay, only a subset of the known monikers make sense, and they are shown here.
+        // Other images can be used, but should be tested on a case-by-case basis. The expectation
+        // is that the overlay appears in the lower-right corner of the icon.
+        private static readonly ImageMoniker[] s_overlays =
+        {
+            KnownMonikers.OverlayAlert,
+            KnownMonikers.OverlayError,
+            KnownMonikers.OverlayExcluded,
+            KnownMonikers.OverlayFriend,
+            KnownMonikers.OverlayLock,
+            KnownMonikers.OverlayLoginDisabled,
+            KnownMonikers.OverlayWarning
+        };
+        
+        // For the state icon, only a subset of the known monikers work. These are smaller (7x16) than most other monikers (16x16).
+        // Attempting to use a 16x16 icon here will result in an icon that appears squashed and likely illegible.
+        private static readonly ImageMoniker[] s_states =
+        {
+            KnownMonikers.CheckedInNode,
+            KnownMonikers.CheckedOutForEditNode,
+            KnownMonikers.CheckedOutByOtherUserNode,
+            KnownMonikers.PendingChangeNode,
+            KnownMonikers.PendingDeleteNode,
+            KnownMonikers.PendingMergeNode,
+            KnownMonikers.PendingRenameNode,
+            KnownMonikers.PendingUndeleteNode,
+            KnownMonikers.SourceControlEditableNode,
+            KnownMonikers.SourceControlExcludedNode,
+            KnownMonikers.SourceControlOrphanedNode
+        };
+        
         private static readonly Random s_random = new Random();
+        
         private static ImageMoniker SelectRandom(ImageMoniker[] monikers) => monikers[s_random.Next(0, monikers.Length)];
 
         #endregion
